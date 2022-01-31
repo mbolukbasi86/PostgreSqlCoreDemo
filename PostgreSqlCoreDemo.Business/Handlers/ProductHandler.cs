765782f76db9a41ae60b777f73687f0eb244e3d9
Murat Bolukbasi <mbolukbasi@protel.com.tr>
2022-01-31 12:50:50 +0300

    ...

--- PostgreSqlCoreDemo.Business/Handlers/ProductHandler.cs
+++ PostgreSqlCoreDemo.Business/Handlers/ProductHandler.cs
@@ -21,27 +21,8 @@ namespace PostgreSqlCoreDemo.Business.Handlers
 
         public void AddProduct(Product product)
         {
-            var _product = new Product();
-            _product.ProductID = product.ProductID;
-            _product.ProductName = product.ProductName;
-            _product.SupplierId = product.SupplierId;
-            _product.CategoryId = product.CategoryId;
-            _product.QuantityPerUnit = product.QuantityPerUnit;
-            _product.UnitPrice = product.UnitPrice;
-            _product.UnitsInStock = product.UnitsInStock;
-            _product.UnitsOnOrder = product.UnitsOnOrder;
-            _product.ReorderLevel = product.ReorderLevel;
-            _product.Discontinued = product.Discontinued;
-
-            int discontinued = Convert.ToInt32(_product.Discontinued);
-            DbHelper.ExecuteNonQuery($"INSERT INTO Products values( {_product.ProductID},'{_product.ProductName}', {_product.SupplierId}, {_product.CategoryId}, '{_product.QuantityPerUnit}', {_product.UnitPrice}, {_product.UnitsInStock}, {_product.UnitsOnOrder}, {_product.ReorderLevel}, {discontinued})");
-
-
-            //DbHelper.ExecuteNonQuery("INSERT INTO Products (ProductName,SupplierId,CategoryId,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsInOrder,ReorderLevel,Discontinued) " +
-            //    " values(@ProductName, @SupplierId, @CategoryId, @QuantityPerUnit, @UnitPrice, @UnitsInStock, @UnitsOnOrder, @ReorderLevel, @Discontinued )", _product);
-
-            //DbHelper.ExecuteNonQuery("INSERT INTO Products values(@ProductID, @ProductName, @SupplierId, @CategoryId, @QuantityPerUnit, @UnitPrice, @UnitsInStock, @UnitsOnOrder, @ReorderLevel, @Discontinued )", _product);
-
+            int discontinued = Convert.ToInt32(product.Discontinued);
+            DbHelper.ExecuteNonQuery($"INSERT INTO Products values( {product.ProductID},'{product.ProductName}', {product.SupplierId}, {product.CategoryId}, '{product.QuantityPerUnit}', {product.UnitPrice}, {product.UnitsInStock}, {product.UnitsOnOrder}, {product.ReorderLevel}, {discontinued})");
         }
     }
 }
